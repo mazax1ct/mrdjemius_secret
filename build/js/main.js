fc2c818e633459ceab70c0c4f2b74d5341f50ae9
mazax1ct <vova@giveandget.ru>
2021-03-22 13:36:22 +0300

    remake js

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,16 @@
-function clipboardTextCopyFunction() {
-  var copyText = document.getElementById("promoCode");
-  document.execCommand("copy");
-  alert("Промокод скопирован: " + copyText.value);
-}
+var copyTextareaBtn = document.querySelector('.js-textareacopybtn');
+//var buttonText = document.querySelector('.js-copybtn');
+
+copyTextareaBtn.addEventListener('click', function(event) {
+  var copyTextarea = document.querySelector('.js-copytextarea');
+  copyTextarea.focus();
+  copyTextarea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'Промокод скопирован' : 'Промокод не скопирован. Обратитетсь в службу поддержки';
+    alert(msg);
+  } catch (err) {
+    alert('Что-то пошло не так. Скопируйте текст вручную');
+  }
+});
